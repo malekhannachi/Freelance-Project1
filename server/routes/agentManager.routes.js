32a8e8e918b5afeaa560078ad1d3c17b25c1c25d
Themri Montassar <montassar.themri1@gmail.com>
2022-07-04 08:18:21 +0100

    ****

--- server/routes/agentManager.routes.js
+++ server/routes/agentManager.routes.js
@@ -1,11 +1,31 @@
 const {
   addAgent,
   getAgents,
+  getAgent,
 } = require("../controllers/agentManager.controllers");
+const User = require("../models/user.models");
 
 const router = require("express").Router();
 
+
+router.param("agent", async (req, res, next, id) => {
+    try {
+      const agent = await User.findById(id);
+  console.log(agent.role)
+      if (!agent || agent.role==="admin") {
+        return res.status(404).json("not found agent");
+      } else {
+        req.product = product;
+        next();
+      }
+    } catch (err) {
+      return res.status(500).json(err);
+    }
+  });
+
+
 router.post("/", addAgent);
 router.get("/get", getAgents);
+router.get("/:agent", getAgent);
 
 module.exports = router;
