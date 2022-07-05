95870beaaec8957dc66f665534f1d2c1115eebbd
malekhannachi <malek.hannachi05@gmail.com>
2022-07-05 09:19:26 +0100

    add fournisseur , get all,get one ,delete

--- client/src/app/components/private/admin/fournisseurs/add-fournisseur/add-fournisseur.component.ts
+++ client/src/app/components/private/admin/fournisseurs/add-fournisseur/add-fournisseur.component.ts
@@ -1,15 +1,64 @@
 import { Component, OnInit } from '@angular/core';
+import {
+  FormGroup,
+  FormBuilder,
+  FormControl,
+  Validators,
+} from '@angular/forms';
+import { Router } from '@angular/router';
+import { Fournisseur } from 'src/app/models/fournisseur';
+import { FournisseurService } from 'src/app/services/fournisseur.service';
 
 @Component({
   selector: 'app-add-fournisseur',
   templateUrl: './add-fournisseur.component.html',
-  styleUrls: ['./add-fournisseur.component.css']
+  styleUrls: ['./add-fournisseur.component.css'],
 })
 export class AddFournisseurComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+  fourForm!: FormGroup;
+  constructor(
+    private fb: FormBuilder,
+    private router: Router,
+    private fs: FournisseurService
+  ) {
+    let formControls = {
+      firstname: new FormControl('', [Validators.required]),
+      lastname: new FormControl('', [Validators.required]),
+      email: new FormControl('', [Validators.required, Validators.email]),
+      cin: new FormControl('', [Validators.required]),
+      number: new FormControl('', [Validators.required]),
+      street: new FormControl('', [Validators.required]),
+      city: new FormControl('', [Validators.required]),
+      country: new FormControl('', [Validators.required]),
+      zipCode: new FormControl('', [Validators.required]),
+    };
+    this.fourForm = this.fb.group(formControls);
   }
 
+  ngOnInit(): void {}
+
+  addFourni() {
+    let data = this.fourForm.value;
+    console.log(data);
+
+    let fournisseur = new Fournisseur(
+      undefined,
+      data.firstname,
+      data.lastname,
+      data.email,
+      data.cin,
+      data.number,
+      data.street,
+      data.city,
+      data.country,
+      data.zipCode
+    );
+    console.log(fournisseur);
+
+    this.fs.addFournisseur(fournisseur).subscribe((res) => {
+      console.log(res);
+
+      this.router.navigate(['/list-fournisseur']);
+    });
+  }
 }
--- client/src/app/components/private/admin/fournisseurs/update-fournisseur/update-fournisseur.component.ts
+++ client/src/app/components/private/admin/fournisseurs/update-fournisseur/update-fournisseur.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, FormControl, Validators } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Fournisseur } from 'src/app/models/fournisseur';
+import { FournisseurService } from 'src/app/services/fournisseur.service';
 
 @Component({
   selector: 'app-update-fournisseur',
@@ -6,10 +10,75 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./update-fournisseur.component.css']
 })
 export class UpdateFournisseurComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+  fourForm!: FormGroup;
+  id: any;
+  constructor(
+    private fb: FormBuilder,
+    private router: Router,
+    private route: ActivatedRoute,
+    private fs: FournisseurService
+  ) {
+    let formControls = {
+      firstname: new FormControl('', [Validators.required]),
+      lastname: new FormControl('', [Validators.required]),
+      email: new FormControl('', [Validators.required, Validators.email]),
+      cin: new FormControl('', [Validators.required]),
+      number: new FormControl('', [Validators.required]),
+      street: new FormControl('', [Validators.required]),
+      city: new FormControl('', [Validators.required]),
+      country: new FormControl('', [Validators.required]),
+      zipCode: new FormControl('', [Validators.required]),
+    };
+    this.fourForm = this.fb.group(formControls);
   }
 
+  ngOnInit(): void {
+    let idUser = this.route.snapshot.params['id'];
+    this.id = idUser;
+    console.log(this.id);
+    console.log(idUser);
+
+    this.fs.getbyId(idUser).subscribe((result) => {
+      let user = result;
+      console.log(user);
+      this.fourForm.patchValue({
+        firstname: user.firstName,
+        lastname: user.lastName,
+        email: user.email,
+        cin: user.cin,
+        number: user.number,
+        street: user.address.street,
+        city: user.address.city,
+        country: user.address.country,
+        zipCode:user.address.zipCode,
+       
+      });
+    });
+
+
+  }
+
+  updateFourni() {
+    let data = this.fourForm.value;
+    console.log(data);
+
+    let fournisseur = new Fournisseur(
+      undefined,
+      data.firstname,
+      data.lastname,
+      data.email,
+      data.cin,
+      data.number,
+      data.street,
+      data.city,
+      data.zipCode
+    );
+    console.log(fournisseur);
+
+    this.fs.addFournisseur(fournisseur).subscribe((res) => {
+      console.log(res);
+
+      this.router.navigate(['/list-fournisseur']);
+    });
+  }
 }
