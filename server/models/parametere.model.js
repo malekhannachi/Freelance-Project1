95829fe27720312fda130cf42ffbc1e8320425fa
Montassar <montassarthemri@jendouba.r-iset.tn>
2022-08-31 22:24:51 +0100

    fixe bug

--- server/models/parametere.model.js
+++ server/models/parametere.model.js
@@ -4,12 +4,7 @@ const parametereSchema = new mongoose.Schema(
     identifier: { type: Number, default: 0 },
     parametres: { type: String, required: true },
 
-    limitesDacceptation: [
-      {
-        min: { type: Number },
-        max: { type: Number, min: 1 },
-      },
-    ],
+    limitesDacceptation: { type: String, enum: ["negatif", "positif"] },
   },
   { timestamps: true }
 );
