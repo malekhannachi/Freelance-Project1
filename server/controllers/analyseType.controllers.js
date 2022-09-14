aa7b5ee273cc8cc44cefbf31dce9c8765028b589
Themri Montassar <montassar.themri1@gmail.com>
2022-09-14 06:06:00 +0100

    fix update analyse

--- server/controllers/analyseType.controllers.js
+++ server/controllers/analyseType.controllers.js
@@ -24,10 +24,8 @@ const addAnalyse = async (req, res) => {
     temperature > 10 ||
     densite < 1028 ||
     matiereGrasse < 30 ||
-    ESD > 100 
-    ||
-    congelation != -0.52
-    ||
+    ESD > 100 ||
+    congelation != -0.52 ||
     pourcentageEau < 1.5 ||
     pourcentageEau > 5 ||
     acidite < 14 ||
@@ -123,10 +121,75 @@ const deleteAnalyse = async (req, res) => {
 const updateAnalyse = async (req, res) => {
   const id = req.analyse._id;
 
+  temperature = req.body.temperature;
+  densite = req.body.densite;
+  matiereGrasse = req.body.matiereGrasse;
+  ESD = req.body.ESD;
+  congelation = req.body.congelation;
+  pourcentageEau = req.body.pourcentageEau;
+  acidite = req.body.acidite;
+  PH = req.body.PH;
+  alcool = req.body.alcool;
+  formol = req.body.formol;
+  testAmidon = req.body.testAmidon;
+  antibiotique = req.body.antibiotique;
+
+  fournisseur = req.body.fournisseur;
+  camion = req.body.camion;
+  citerne = req.body.citerne;
+  GoutEtOdeur = req.body.GoutEtOdeur;
+
+  let decision = "";
+  if (
+    temperature > 10 ||
+    densite < 1028 ||
+    matiereGrasse < 30 ||
+    ESD > 100 ||
+    congelation != -0.52 ||
+    pourcentageEau < 1.5 ||
+    pourcentageEau > 5 ||
+    acidite < 14 ||
+    acidite > 16 ||
+    PH < 6.6 ||
+    PH > 6.7 ||
+    alcool === "positif" ||
+    formol === "positif" ||
+    testAmidon === "positif" ||
+    antibiotique === "positif" ||
+    GoutEtOdeur === "non"
+  ) {
+    decision = "Refuse";
+  } else {
+    decision = "Accepte";
+  }
+
   try {
-    const updateAnalyse = await Analyse.findByIdAndUpdate(id, req.body, {
-      new: true,
-    });
+    const updateAnalyse = await Analyse.findByIdAndUpdate(
+      id,
+      {
+        temperature: temperature,
+        densite: densite,
+        matiereGrasse: matiereGrasse,
+        ESD: ESD,
+        congelation: congelation,
+        pourcentageEau: pourcentageEau,
+        acidite: acidite,
+        PH: PH,
+        alcool: alcool,
+        formol: formol,
+        testAmidon: testAmidon,
+        decision: decision,
+        antibiotique: antibiotique,
+        fournisseur: fournisseur,
+        camion: camion,
+        citerne: citerne,
+        GoutEtOdeur: GoutEtOdeur,
+        malade: malade,
+      },
+      {
+        new: true,
+      }
+    );
     return res.status(200).json(updateAnalyse);
   } catch (err) {
     return res.status(500).json(err);
