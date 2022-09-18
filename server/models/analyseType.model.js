3fa45c6d0a0bb6580c088bfd4b96b4c8eba611cf
Montassar <montassarthemri@jendouba.r-iset.tn>
2022-09-18 22:22:58 +0100

    fix iD

--- server/models/analyseType.model.js
+++ server/models/analyseType.model.js
@@ -1,46 +1,99 @@
 const mongoose = require("mongoose");
 const counter = require("./counter.model")
-const AnalysisTypeSchema = new mongoose.Schema(
-  {
-    id: { type: Number, default: 0 },
-    analyseNumber: { type: Number, default: 0 },
-
-    temperature: { type: Number /*required: true*/ },
-
-    densite: { type: Number },
-
-    matiereGrasse: { type: Number, /*required: true,*/ max: 99999999 },
-
-    ESD: { type: Number, max: 101 },
-
-    congelation: { type: Number /*required: true*/ },
-
-    pourcentageEau: { type: Number },
-
-    acidite: { type: Number },
-
-    PH: { type: Number },
-
-    alcool: { type: String, enum: ["negatif", "positif"] },
-
-    formol: { type: String, enum: ["negatif", "positif"] },
-
-    testAmidon: { type: String, enum: ["negatif", "positif"] },
-
-    antibiotique: { type: String, enum: ["negatif", "positif"] },
-
-    fournisseur: { type: mongoose.Schema.Types.ObjectId, ref: "Fournisseur" },
-
-    camion: { type: mongoose.Schema.Types.ObjectId, ref: "Camion" },
-
-    citerne: { type: mongoose.Schema.Types.ObjectId, ref: "Citerne" },
-
-    decision: { type: String, enum: ["Accepte", "Refuse"] },
-    GoutEtOdeur: { type: String, enum: ["oui", "non"] },
-    malade: { type: Array, default: [] },
+const AnalysisTypeSchema = new mongoose.Schema({
+  id: {
+    type: Number,
+    default: 0
   },
-  { timestamps: true }
-);
+  analyseNumber: {
+    type: Number,
+    default: 0
+  },
+
+  temperature: {
+    type: Number /*required: true*/
+  },
+
+  densite: {
+    type: Number
+  },
+
+  matiereGrasse: {
+    type: Number,
+    /*required: true,*/ max: 99999999
+  },
+
+  ESD: {
+    type: Number,
+    max: 101
+  },
+
+  congelation: {
+    type: Number /*required: true*/
+  },
+
+  pourcentageEau: {
+    type: Number
+  },
+
+  acidite: {
+    type: Number
+  },
+
+  PH: {
+    type: Number
+  },
+
+  alcool: {
+    type: String,
+    enum: ["negatif", "positif"]
+  },
+
+  formol: {
+    type: String,
+    enum: ["negatif", "positif"]
+  },
+
+  testAmidon: {
+    type: String,
+    enum: ["negatif", "positif"]
+  },
+
+  antibiotique: {
+    type: String,
+    enum: ["negatif", "positif"]
+  },
+
+  fournisseur: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Fournisseur"
+  },
+
+  camion: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Camion"
+  },
+
+  citerne: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Citerne"
+  },
+
+  decision: {
+    type: String,
+    enum: ["Accepte", "Refuse"]
+  },
+  GoutEtOdeur: {
+    type: String,
+    enum: ["oui", "non"]
+  },
+  malade: {
+    type: Array,
+    default: []
+  },
+}, {
+  timestamps: true
+});
 
 
 
@@ -52,4 +105,6 @@ AnalysisTypeSchema.pre('save', function (next) {
 
   counter('activities', this, next);
 });
+
+
 module.exports = mongoose.model("AnalysisType", AnalysisTypeSchema);
